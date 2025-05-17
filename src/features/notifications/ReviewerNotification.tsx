50c959f8e6e8769c40db1afb9a986f92da2182a4
Mikita Bazhkou <mikitabazhkou1@gmail.com>
2025-05-17 11:48:58 +0200

    fix warnings

--- src/features/notifications/ReviewerNotification.tsx
+++ src/features/notifications/ReviewerNotification.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { JsonEditor } from '../../components/JsonEditor';
 import { PersonSelector } from '../../components/PersonSelector';
 import { apiService } from '../../services/apiService';
